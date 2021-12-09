4328b7b5b834651ce194edb512b4bd43813bc861
Michael-Otieno <michael.otieno@student.moringaschool.com>
2021-12-09 22:08:22 +0300

    add variables to be used

--- js/script.js
+++ js/script.js
@@ -3,5 +3,8 @@ function getName(){
     const maleNames=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     const femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
+    let dayOfBirth = parseInt(document.querySelector('.day').value)
+    let monthOfBirth = parseInt(document.querySelector('.month').value)
+    let yearOfBirth = parseInt(document.querySelector('.year').value)
     
 }
