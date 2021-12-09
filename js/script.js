d610b0071ec664b8ddae7cd2c073c8c8a88b0de5
Michael-Otieno <michael.otieno@student.moringaschool.com>
2021-12-09 17:17:36 +0300

    add project

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function getName(){
+    const weekDays=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const maleNames=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    const femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+    
+}
