7f270334fc42e0a81cc14a65835fc511c5c0673a
Michael-Otieno <michael.otieno@student.moringaschool.com>
2021-12-16 01:26:50 +0300

    add js function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,153 @@
+function fetchData (){
+
+    var name = document.getElementById("userName").value;
+
+    var DD = document.getElementById("dateOnly").value;
+
+    var MM = document.getElementById("monthOnly").value;
+
+    var fullYear = document.getElementById("yearOnly").value;
+
+
+    // spliting full-year into century and year
+
+    var fullYearString = fullYear.toString();
+
+    var CC = parseInt(fullYearString.slice(0,2));
+
+    var YY = parseInt(fullYearString.slice(2,4));
+
+    var day = (((CC/4) -2*CC-1) + ((5*YY/4)) + ((26*(MM+1)/10)) + DD )%7
+
+    var birthDay = parseInt(day);
+
+    // radio button controls validation
+
+    var male = document.getElementById("maleGender").checked;
+
+    var female = document.getElementById("femaleGender").checked;
+
+    // form validation
+
+    // radio buttons
+    if(male == false && female == false){
+        alert("Please select a gender!")
+        return false;
+    };
+
+    // date of birth
+    if(DD < 1 || DD >31){
+        alert("Enter valid date. \nIt should range from 1 to 31.")
+        return false;
+    };
+
+    // month
+    if(MM < 1 || MM > 12){
+        alert("Enter a valid month. \nIt should range from 1 to 12.")
+        return false;
+    };
+
+    // months with 30 days
+    if(( MM == 4 || MM == 6 || MM == 9 || MM == 11) && DD > 30){
+        alert("Please Enter a valid date. \n"+MM+ "th month has NO MORE THAN 30 days!")
+        return false;
+    };
+
+    // Month of February
+    if((MM == 2 && DD > 28) && (fullYear % 4 != 0)){
+        alert("Enter a valid date! \nFebruary has no more than 28 days.")
+        return false;
+    };
+
+    if((MM == 2 && fullYear % 4 == 0) && (DD > 29)){
+        alert("Enter a valid date! \n"+fullYear+" is a leap year! It has 29 days.")
+        return false;
+    }
+
+    // year validation
+    if(fullYear.length < 4 || fullYear.length > 4 ){
+        alert("Please enter a valid Year! \nA year has 4 figures.")
+        return false;
+    };
+
+    if(CC >= 21){
+        alert("Jesus Christ!!! Enter a valid year! \nWe are still in 20s.")
+        return false;
+    };
+
+
+    // Arrays
+
+    var daysArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    var malesArray = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+
+    var femaleArray = ['Akosua', 'Adwoa', 'Abena', 'Akua', 'Yaa', 'Afua', 'Ama'];
+
+    // Assigning names using if statement
+
+    // Males
+
+    if( birthDay == 0 && male == true){
+        alert ("Hello "+name+", \nYou were born on: "+daysArray[0]+", \nyour Akan name is: "+malesArray[0]);
+    };
+
+    if( birthDay == 1 && male == true){
+        alert ("Hello "+name+", \nYou were born on: "+daysArray[1]+", \nyour Akan name is: "+malesArray[1]);
+    };
+
+    if( birthDay == 2 && male == true){
+        alert ("Hello "+name+", \nYou were born on: "+daysArray[2]+", \nyour Akan name is: "+malesArray[2]);
+    };
+
+    if( birthDay == 3 && male == true){
+        alert ("Hello "+name+", \nYou were born on: "+daysArray[3]+", \nyour Akan name is: "+malesArray[3]);
+    };
+
+    if( birthDay == 4 && male == true){
+        alert ("Hello "+name+", \nYou were born on: "+daysArray[4]+", \nyour Akan name is: "+malesArray[4]);
+    };
+
+    if( birthDay == 5 && male == true){
+        alert ("Hello "+name+", \nYou were born on: "+daysArray[5]+", \nyour Akan name is: "+malesArray[5]);
+    };
+
+    if( birthDay == 6 && male == true){
+        alert ("Hello "+name+", \nYou were born on: "+daysArray[6]+", \nyour Akan name is: "+malesArray[6]);
+    };
+
+    // Females
+
+    if( birthDay == 0 && female == true){
+        alert ("Hello "+name+", \nYou were born on: "+daysArray[0]+", \nyour Akan name is: "+femaleArray[0]);
+    };
+
+    if( birthDay == 1 && female == true){
+        alert ("Hello "+name+", \nYou were born on: "+daysArray[1]+", \nyour Akan name is: "+femaleArray[1]);
+    };
+
+    if( birthDay == 2 && female == true){
+        alert ("Hello "+name+", \nYou were born on: "+daysArray[2]+", \nyour Akan name is: "+femaleArray[2]);
+    };
+
+    if( birthDay == 3 && female == true){
+        alert ("Hello "+name+", \nYou were born on: "+daysArray[3]+", \nyour Akan name is: "+femaleArray[3]);
+    };
+
+    if( birthDay == 4 && female == true){
+        alert ("Hello "+name+", \nYou were born on: "+daysArray[4]+", \nyour Akan name is: "+femaleArray[4]);
+    };
+
+    if( birthDay == 5 && female == true){
+        alert ("Hello "+name+", \nYou were born on: "+daysArray[5]+", \nyour Akan name is: "+femaleArray[5]);
+    };
+
+    if( birthDay == 6 && female == true){
+        alert ("Hello "+name+", \nYou were born on: "+daysArray[6]+", \nyour Akan name is: "+femaleArray[6]);
+    };
+
+};
+ 
    
    
    
@@ -5,204 +155,3 @@
    
    
    
-   
-   
-   
-   
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   
-   // let dayOfBirth = document.querySelector('.day');
-    // let monthOfBirth = document.querySelector('.month');
-    // let yearOfBirth = document.querySelector('.year');
-
-        // spliting full-year into century and year
-    // let year = yearOfBirth.toString()
-    // let cc = parseInt(year.slice(0,2));
-    // let yy = parseInt(year.slice(2,4));
-
-
-// function getName() {
-//     alert('I am a placeholder');
-//   }
-  
-// var getName = function (){
-   
-//     let dayOfBirth = document.getElementsById('day').value;
-//     let monthOfBirth = document.getElementsById('month').value;
-//     let yearOfBirth = document.getElementsById('year').value;
-
-//         // spliting full-year into century and year
-//     let year = yearOfBirth.toString()
-//     let cc = parseInt(year.slice(0,2));
-//     let yy = parseInt(year.slice(2,4));
-
-//     let day = (((cc/4) -2*cc-1) + ((5*yy/4)) + ((26*(monthOfBirth+1)/10)) + dayOfBirth )%7;
-
-//     let birthday = parseInt(day);
-
-//     // radio button controls validation
-//     let male = document.getElementsById('male').checked
-//     let female = document.getElementsById('female').checked
-
-//     // form validation
-
-//     //radio buttons validation
-//     if(male==false && female==false){
-//         alert('Select gender')
-//         return false;
-//     }
-    
-//     // date of birth
-//     if(dayOfBirth < 1 || dayOfBirth > 31){
-//         alert('Enter a valid date')
-//         return false;
-//     }
-
-//     // month of birth
-//     if(monthOfBirth < 1 || monthOfBirth > 12){
-//         alert('Enter a valid month')
-//         return false
-//     }
-
-//     // month of birth with 30 days
-//     if((monthOfBirth==4 || monthOfBirth==6 || monthOfBirth==9 || monthOfBirth==11) && dayOfBirth > 30){
-//         alert('Enter a valid date')
-//         return false
-//     }
-
-//     // month of february
-//     if((monthOfBirth==2 && dayOfBirth>28) && (yearOfBirth%4 != 0)){
-//         alert('February has 28 days')
-//         return false
-//     }
-
-//     if((monthOfBirth==2 && yearOfBirth%4 != 0) && (dayOfBirth>29)){
-//         alert('February has 29 days')
-//         return false
-//     }
-
-//     // year validation
-//     if(yearOfBirth.length < 4  || yearOfBirth.length > 4){
-//         alert('Enter a valid year')
-//         return false
-//     }
-
-//     if(cc>=21){
-//         alert('Enter a 21st century year')
-//         return false;
-//     }
-
-//     var weekDays=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-//     var maleNames=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-//     var femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-
-//        // Assigning names using if statement
-
-//     // Males
-
-//     if( birthday == 0 && male == true){
-//         alert ("Hello "+", \nYou were born on: "+weekDays[0]+", \nyour Akan name is: "+maleNames[0]);
-//     };
-
-//     if( birthday == 1 && male == true){
-//         alert ("Hello "+", \nYou were born on: "+weekDays[1]+", \nyour Akan name is: "+maleNames[1]);
-//     };
-
-//     if( birthday == 2 && male == true){
-//         alert ("Hello "+", \nYou were born on: "+weekDays[2]+", \nyour Akan name is: "+maleNames[2]);
-//     };
-
-//     if( birthday == 3 && male == true){
-//         alert ("Hello "+", \nYou were born on: "+weekDays[3]+", \nyour Akan name is: "+maleNames[3]);
-//     };
-
-//     if( birthday == 4 && male == true){
-//         alert ("Hello "+", \nYou were born on: "+weekDays[4]+", \nyour Akan name is: "+maleNames[4]);
-//     };
-
-//     if( birthDay == 5 && male == true){
-//         alert ("Hello "+", \nYou were born on: "+weekDays[5]+", \nyour Akan name is: "+maleNames[5]);
-//     };
-
-//     if( birthday == 6 && male == true){
-//         alert ("Hello "+", \nYou were born on: "+weekDays[6]+", \nyour Akan name is: "+maleNames[6]);
-//     };
-
-//     // Females
-
-//     if( birthday == 0 && female == true){
-//         alert ("Hello "+", \nYou were born on: "+weekDays[0]+", \nyour Akan name is: "+femaleNames[0]);
-//     };
-
-//     if( birthday == 1 && female == true){
-//         alert ("Hello "+", \nYou were born on: "+weekDays[1]+", \nyour Akan name is: "+femaleNames[1]);
-//     };
-
-//     if( birthday == 2 && female == true){
-//         alert ("Hello "+", \nYou were born on: "+weekDays[2]+", \nyour Akan name is: "+femaleNames[2]);
-//     };
-
-//     if( birthday == 3 && female == true){
-//         alert ("Hello "+", \nYou were born on: "+weekDays[3]+", \nyour Akan name is: "+femaleNames[3]);
-//     };
-
-//     if( birthday == 4 && female == true){
-//         alert ("Hello "+", \nYou were born on: "+weekDays[4]+", \nyour Akan name is: "+femaleNames[4]);
-//     };
-
-//     if( birthday == 5 && female == true){
-//         alert ("Hello "+", \nYou were born on: "+weekDays[5]+", \nyour Akan name is: "+maleNames[5]);
-//     };
-
-//     if( birthday == 6 && female == true){
-//         alert ("Hello "+", \nYou were born on: "+weekDays[6]+", \nyour Akan name is: "+maleNames[6]);
-//     };
-
-// }
